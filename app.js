10ccf464740405d44903a7829afbc9b56b299fb1
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-04-15 16:22:54 -0400

    a9

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
 import HelloController
   from "./controllers/hello-controller.js";
 import UserController
   from "./controllers/users/users-controller.js";
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
-  
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,3 +16,4 @@ TuitsController(app);
 HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
+mongoose.connect(CONNECTION_STRING);
